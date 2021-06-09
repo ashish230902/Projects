87fcb36c1bb6b134afdfc6331a6725fb6b181548
ashish230902 <75869438+ashish230902@users.noreply.github.com>
2021-06-09 16:40:13 +0530

    Update ACC_Processing.m

--- ACC_Processing.m
+++ ACC_Processing.m
@@ -1,6 +1,5 @@
-Fs=100;
-g =9.81;
-G=[0,0,9.81];
+Fs=100; %Data Rate
+G=[0,0,9.81]; %Defining Acceleration Vector
 Avalues = table2array( Acceleration); % Acceleration Values
 AvaluesX = (Avalues(:,1)); % Acceleration Values for X
 AvaluesY = (Avalues(:,2)); % Acceleration Values for y
@@ -11,16 +10,14 @@ MvaluesX = (Mvalues(:,1)); % Magnetic Field Values for X
 MvaluesY = (Mvalues(:,2)); % Magnetic Field Values for y
 MvaluesZ = (Mvalues(:,3)); % Magnetic Field Values for z
 
-Vvalues = table2array(AngularVelocity);
-ValuesX = (Vvalues(:,1));
-ValuesY = (Vvalues(:,2));
-ValuesZ = (Vvalues(:,3));
+Vvalues = table2array(AngularVelocity);  % Angular Velocity Values
+ValuesX = (Vvalues(:,1));   % Angular Velocity Values X
+ValuesY = (Vvalues(:,2));   % Angular Velocity Values Y
+ValuesZ = (Vvalues(:,3));   % Angular Velocity Values Z
 
 decim = 2;
-fuse = ahrsFilter('SampleRate',Fs,'DecimationFactor',decim);
+fuse = ahrsFilter('SampleRate',Fs,'DecimationFactor',decim); %Applying AHRS filter to calculate the orientation
 q = fuse(Avalues,Vvalues,Mvalues);
 
-eul = eulerd(q,'ZXY','frame');
-Eul = g*cosd(eul);
-a_rotated = rotateframe(q, Avalues);
-user_acceleration = a_rotated - G;
+a_rotated = rotateframe(q, Avalues);% rotating measured accelration along earth's orientation
+user_acceleration = a_rotated - G; % calculated Linear Acceleration
