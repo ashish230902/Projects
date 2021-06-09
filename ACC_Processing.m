18577b05cb45d7671e6acd91e8b3a8365a4db80b
ashish230902 <75869438+ashish230902@users.noreply.github.com>
2021-06-09 18:22:42 +0530

    Update ACC_Processing.m

--- ACC_Processing.m
+++ ACC_Processing.m
@@ -1,23 +1,45 @@
-Fs=100; %Data Rate
-G=[0,0,9.81]; %Defining Acceleration Vector
+Fs=100;
+x = [1:388]';
+G=[0,0,9.81];
 Avalues = table2array( Acceleration); % Acceleration Values
 AvaluesX = (Avalues(:,1)); % Acceleration Values for X
 AvaluesY = (Avalues(:,2)); % Acceleration Values for y
 AvaluesZ = (Avalues(:,3)); % Acceleration Values for Z
-mag_acc = sqrt(AvaluesX.^2 + AvaluesY.^2 + AvaluesZ.^2);
+
 Mvalues= table2array(MagneticField);  % Magnetic Field Values
 MvaluesX = (Mvalues(:,1)); % Magnetic Field Values for X
 MvaluesY = (Mvalues(:,2)); % Magnetic Field Values for y
 MvaluesZ = (Mvalues(:,3)); % Magnetic Field Values for z
 
-Vvalues = table2array(AngularVelocity);  % Angular Velocity Values
-ValuesX = (Vvalues(:,1));   % Angular Velocity Values X
-ValuesY = (Vvalues(:,2));   % Angular Velocity Values Y
-ValuesZ = (Vvalues(:,3));   % Angular Velocity Values Z
+Vvalues = table2array(AngularVelocity);
+ValuesX = (Vvalues(:,1));
+ValuesY = (Vvalues(:,2));
+ValuesZ = (Vvalues(:,3));
 
-decim = 2;
-fuse = ahrsFilter('SampleRate',Fs,'DecimationFactor',decim); %Applying AHRS filter to calculate the orientation
+fuse = ahrsfilter('SampleRate',Fs,'DecimationFactor', 2);
 q = fuse(Avalues,Vvalues,Mvalues);
+n=2 ;
+q=repmat(q,1,n)';
+q=q(:)'
 
-a_rotated = rotateframe(q, Avalues);% rotating measured accelration along earth's orientation
-user_acceleration = a_rotated - G; % calculated Linear Acceleration
+
+a_rotated = rotateframe(q, Avalues);
+user_acceleration = a_rotated - G;
+
+figure
+
+plot(x,AvaluesX,x,user_acceleration(:,1))
+title(' X-Axis')
+legend({'y = Accelerometer','y = Linear acceleration'})
+
+figure
+
+plot(x,AvaluesY,x,user_acceleration(:,2))
+title('Y-Axis')
+legend({'y = Accelerometer','y = Linear acceleration'})
+
+figure
+
+plot(x,AvaluesZ,x,user_acceleration(:,3))
+title('Z-Axis')
+legend({'y = Accelerometer','y = Linear acceleration'})
